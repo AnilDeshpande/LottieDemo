80b55758d19804a5052fb038a57488af18337339
Anil Deshpande <anildesh82@gmail.com>
2019-11-21 12:16:59 +0530

    splash animation added

--- app/src/main/java/com/codetutor/lottiedemo/MainActivity.java
+++ app/src/main/java/com/codetutor/lottiedemo/MainActivity.java
@@ -6,19 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieDrawable;
 
 public class MainActivity extends AppCompatActivity {
+
+    LottieAnimationView lottieAniView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lottieAniView = (LottieAnimationView) findViewById(R.id.lottieAniView);
+        lottieAniView.setRepeatCount(LottieDrawable.INFINITE);
+
+        Log.i("Lottie","Animation Duration: "+lottieAniView.getDuration());
+
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(3000);
+                    Thread.sleep(5000);
                     MainActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
